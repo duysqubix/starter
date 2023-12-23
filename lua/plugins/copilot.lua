39111e459c0c39f2a70f1071c1d71082b8869011
duysqubix <duys@qubixds.com>
2023-12-23 16:28:56 -0600

    "Updated README.md to include new installation instructions. This change was necessary to reflect the recent updates in the software's installation process."

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "zibirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = { accept = "<F9>" },
+      },
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>",
+        },
+        layout = {
+          position = "bottom", -- | top | left | right
+          ratio = 0.4,
+        },
+      },
+      filestypes = {
+        markdown = true,
+        help = true,
+        python = true,
+        go = true,
+        rust = true,
+        bash = true,
+        lua = true,
+      },
+    },
+  },
+}
